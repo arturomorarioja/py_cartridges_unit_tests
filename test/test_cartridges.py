c6e0b3144dec05f707323604fbad9994b79d3ee7
Arturo Mora-Rioja <arturomora@gmail.com>
2025-09-02 15:56:19 +0200

    Duplicated tests removed

--- test/test_cartridges.py
+++ test/test_cartridges.py
@@ -16,9 +16,7 @@ class TestCartridges():
         (99, 0.0),      # Partition 5-99: upper valid boundary
         (100, 0.2),     # Partition 100-MAX INTEGER: lower valid boundary
         (101, 0.2),
-        (167, 0.2),
-        (167.3, 0.2),   # Edge case: implies float to int conversion
-        ("167", 0.2)    # Edge case: implies string to int conversion
+        (167, 0.2)
     ])
     def test_cartridges_passes(self, cartridges, discount):
         assert calculate_discount(cartridges) == discount
@@ -28,7 +26,7 @@ class TestCartridges():
     #   
 
     @pytest.mark.parametrize('cartridges', [
-        (-15), (-2), (-1),      # Invalid equivalence partition: MIN INTEGER- -1
+        (-15), (-2), (-1),      # Invalid equivalence partition: MIN INTEGER - -1
         (0),                    # Invalid equivalence partition: 0
         (1), (2), (3), (4),     # Invalid equivalence partition: 1-4
         (-10), (-167)           # Edge cases
